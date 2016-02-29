6ced6ab250db65c2215b4118cdec319e46463767
Valera <mkoctuk@gmail.com>
2016-02-29 13:30:11 +0300

    release

--- kpp.go
+++ kpp.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -29,12 +31,8 @@ func urlEncoded(str string) (string, error) {
 	return u.String(), nil
 }
 
-func getHTML(name string, year int64) ([]byte, error) {
+func getHTML(url string) ([]byte, error) {
 	var body []byte
-	url, err := urlEncoded(apiURL + strings.Replace(name, " ", "+", -1) + "+" + fmt.Sprintf("%d", year) + "/")
-	if err != nil {
-		return body, err
-	}
 	resp, err := http.Get(url)
 	if err != nil {
 		return body, err
@@ -45,7 +43,7 @@ func getHTML(name string, year int64) ([]byte, error) {
 	}
 	buffer := bytes.NewBufferString("")
 	for _, char := range body {
-		var ch = Utf(char)
+		var ch = toUtf(char)
 		fmt.Fprintf(buffer, "%c", ch)
 	}
 	doc := buffer.Bytes()
@@ -55,10 +53,34 @@ func getHTML(name string, year int64) ([]byte, error) {
 // GetRating - получение рейтингов
 func GetRating(name string, year int64) (KP, error) {
 	var kp KP
-	body, err := getHTML(name, year)
+	yearStr := fmt.Sprintf("%d", year)
+	url, err := urlEncoded(apiURL + strings.Replace(name, " ", "+", -1) + "+" + yearStr + "/view/movie/")
 	if err != nil {
 		return kp, err
 	}
-	fmt.Println(string(body))
+	body, err := getHTML(url)
+	if err != nil {
+		return kp, err
+	}
+	resultStr := name + ", " + yearStr
+	reHref := regexp.MustCompile(`<a href="(.*?)">` + resultStr + `<\/a>`)
+	reK := regexp.MustCompile(`<b>рейтинг фильма:</b>.*?<i>(.*?)</i>`)
+	reI := regexp.MustCompile(`<b>рейтинг IMDB:</b>.*?<i>(.*?)</i>`)
+	if reHref.Match(body) == true {
+		findHref := reHref.FindSubmatch(body)
+		href := string(findHref[1])
+		body, err = getHTML(href)
+		if err != nil {
+			return kp, err
+		}
+		if reK.Match(body) == true {
+			kindK := reK.FindSubmatch(body)
+			kp.Kinopoisk, _ = strconv.ParseFloat(string(kindK[1]), 64)
+		}
+		if reI.Match(body) == true {
+			kindI := reI.FindSubmatch(body)
+			kp.IMDb, _ = strconv.ParseFloat(string(kindI[1]), 64)
+		}
+	}
 	return kp, nil
 }
